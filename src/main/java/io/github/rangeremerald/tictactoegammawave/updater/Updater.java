7e6957057b6f21a14b56200841aca9c48264df7b
The Moon That Rises <58153205+RangerEmerald@users.noreply.github.com>
2021-07-18 21:47:48 -0600

    Update Updater.java

--- src/main/java/io/github/rangeremerald/tictactoegammawave/updater/Updater.java
+++ src/main/java/io/github/rangeremerald/tictactoegammawave/updater/Updater.java
@@ -2,22 +2,29 @@ package io.github.rangeremerald.tictactoegammawave.updater;
 
 import io.github.rangeremerald.tictactoegammawave.Private;
 
+import java.net.UnknownHostException;
 import java.security.MessageDigest;
 
 public class Updater {
 
     public Updater() throws Exception {
+        String selfFilePath = Updater.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+
         MessageDigest md = MessageDigest.getInstance("SHA");
-        String shaGame = new CheckSum().checkSum(Updater.class.getProtectionDomain().getCodeSource().getLocation().getPath(), md);
+        String shaGame = new CheckSum().checkSum(selfFilePath, md);
 
-        String newGame = new HttpUrlConnection().httpUrlConnection(new Private().getServerUrl() + shaGame);
+        String url = new Private().getServerUrl() + "interacts/updater.php?user_sha1=" + shaGame;
 
-        if (newGame != null) {
-            Process runtime = Runtime.getRuntime().exec("java -jar " + Updater.class.getProtectionDomain().getCodeSource().getLocation().getPath());
-            if (runtime == null) throw new Exception("Updated jar file did not run.");
+        try {
+            String newGame = new HttpUrlConnection().httpUrlConnection(url);
 
-            System.exit(0);
-        }
+            if (newGame != null) {
+                Process runtime = Runtime.getRuntime().exec("java -jar " + selfFilePath);
+                if (runtime == null) throw new Exception("Updated jar file did not run.");
+
+                System.exit(0);
+            }
+        } catch (UnknownHostException exception) { exception.printStackTrace(); }
 
     }
 
