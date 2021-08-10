1b3dc83ddb3c243786c0fee52c033b9bee7588c6
The Moon That Rises <58153205+RangerEmerald@users.noreply.github.com>
2021-08-10 17:40:29 -0600

    Changed SettingsButtons to extend Halfsized Button

--- src/main/java/io/github/rangeremerald/tictactoegammawave/settingsHelper/SettingsButtons.java
+++ src/main/java/io/github/rangeremerald/tictactoegammawave/settingsHelper/SettingsButtons.java
@@ -2,51 +2,22 @@ package io.github.rangeremerald.tictactoegammawave.settingsHelper;
 
 import io.github.rangeremerald.tictactoegammawave.TicTacToe;
 import io.github.rangeremerald.tictactoegammawave.objects.Button;
-import io.github.rangeremerald.tictactoegammawave.objects.ScreenButtonsHelper;
+import io.github.rangeremerald.tictactoegammawave.objects.HalfSizedButton;
+import io.github.rangeremerald.tictactoegammawave.screens.Settings;
 
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.Collections;
-
-public class SettingsButtons extends ScreenButtonsHelper {
-
-    {
-        buttonNames = new ArrayList<>(Collections.singletonList("Exit"));
-
-        rectWidth /= 2;
-        rectHeight /= 2;
-        size /= 2;
-
-        xpos = TicTacToe.settingsScreen.getWidth() / 2 - rectWidth / 2;
-        ypos = TicTacToe.settingsScreen.getHeight() - rectHeight - 30;
-
-    }
+public class SettingsButtons extends HalfSizedButton {
 
     @Override
-    public void updatePos(int width, int height) {
-        xpos = TicTacToe.settingsScreen.getWidth() / 2 - rectWidth / 2;
-        ypos = TicTacToe.settingsScreen.getHeight() - rectHeight - 30;
-    }
-
-    @Override
-    public void updateAnimSpeed(int speed) {
-        buttonHash.get("Exit").buttonAnimation.animSpeed = speed;
-    }
-
-    @Override
-    public boolean initHash(Graphics g) {
-        super.initHash(g);
-        updateAnimSpeed(3);
-        return true;
+    public void initValues() {
+        screenWidth = TicTacToe.settingsScreen.getWidth();
+        screenHeight = TicTacToe.settingsScreen.getHeight();
+        super.initValues();
     }
 
     @Override
     public void whichButton(Button pressedButton, Runnable waitCursor, Runnable defaultCursor) {
         if (pressedButton.drawStringTotal.text.equalsIgnoreCase("exit")) {
-            waitCursor.run();
-            TicTacToe.settingsScreen.dispose();
-            TicTacToe.settingsScreen = null;
-            defaultCursor.run();
+            Settings.SettingsScreen.exitSettings(waitCursor, defaultCursor);
         }
     }
 }
